b7f64ed7d69e635016350f9af87831195fd5c1d5
Mark Holden <mark.holden@monscierge.com>
2021-10-18 00:46:02 -0500

    implement PrintFrequencies

--- ProduceRearrangementAnalyzer/PythonCode.py
+++ ProduceRearrangementAnalyzer/PythonCode.py
@@ -2,7 +2,18 @@ def PrintHistogram():
     print("You printed a histogram!")
 
 def PrintFrequencies():
-    print("You printed the frequency!")
+    print("\nProduce Item Frequencies")
+    for k, v in sorted(GetProduceDict().items(), key=lambda x: x[1], reverse=True):
+        print("Item:", f'{k:16}', "\tQuantity:", v)
 
-def PrintItemFrequency(searchTerm: str):
-    print("You printed the frequency of", searchTerm + ".")
+
+def GetProduceDict() -> dict:
+    produceList = open("ProduceInput.txt")
+    produceDict = {}
+    for inputString in produceList:
+        produce = inputString.rstrip('\n')
+        if produce in produceDict:
+            produceDict[produce] = produceDict[produce] + 1
+        else:
+            produceDict[produce] = 1
+    return produceDict
